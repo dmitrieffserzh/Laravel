fb1379c0303d780a973ae09b9d0f275bdd235567
dmitrieffserzh <dmitrieffserz@yandex.ru>
2018-03-23 13:12:44 +0300

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,4 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js'),
    //.js('resources/assets/js/admin.js', 'public/admin/js'),
    .sass('resources/assets/sass/app.scss', 'public/css'),
-  //.sass('resources/assets/sass/admin.scss', 'public/admin/css');
+   //.sass('resources/assets/sass/admin.scss', 'public/admin/css');
