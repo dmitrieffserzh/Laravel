086ae56690c708675bf2094dd9f23813a0e78aa9
dmitrieffserzh <dmitrieffserz@yandex.ru>
2018-03-23 16:51:01 +0300

    update

--- app/Models/Admin/Category.php
+++ app/Models/Admin/Category.php
@@ -8,10 +8,11 @@ use Illuminate\Support\Str;
 class Category extends Model
 {
 	// Mass assigned
-	protected $fillable = ['title', 'slug', 'parent_id', 'description','sort', 'color', 'published', 'created_at', 'updated_at'];
+	protected $fillable = ['title', 'slug', 'parent_id', 'description', 'sort', 'color', 'published', 'created_at', 'updated_at'];
+
 	// Mutators
 	public function setSlugAttribute($value) {
-		$this->attributes['slug'] = Str::slug( mb_substr($this->title, 0, 40);
+		$this->attributes['slug'] = Str::slug( mb_substr($this->title, 0, 40));
 	}
 	// Get children category
 	public function children() {
